d10e61d80fdea2dbbaa38c2587dc38d4d3368ca8
chgara <chemasanchezgarabito@gmail.com>
2020-03-28 17:44:38 +0100

    Lol

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,10 +1,28 @@
+module.exports = {
+  database: {
+    connectionLimit: 5,
+    host: "bqaiei0yoeufhvurnjou-mysql.services.clever-cloud.com",
+    user: "uhmeud6rdgrrr8ir",
+    password: "ymXMU2fjpZ8W06tLv8Kd",
+    database: "bqaiei0yoeufhvurnjou"
+  }
+};
 /*
+module.exports = {
+    database: {
+        connectionLimit: 5,
+        host: "bqaiei0yoeufhvurnjou-mysql.services.clever-cloud.com",
+        user: "uhmeud6rdgrrr8ir",
+        password: "ymXMU2fjpZ8W06tLv8Kd",
+        database: "bqaiei0yoeufhvurnjou"
+    }
+};/*
 module.exports = {
     database: {
         connectionLimit: 5,
         host: "localhost",
         user: "root",
-        password: "%",
+        password: "airsoftG15935700g%",
         database: "contacts"
     }
 };
